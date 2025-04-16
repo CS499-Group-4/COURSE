b3e6e5b7e26b70a3caa9828889e7cd0ec40d3c30
Elisabeth Elgin <eje0004@uah.edu>
2025-04-16 16:46:12 -0500

    aha

--- pages/homePage.py
+++ pages/homePage.py
@@ -32,67 +32,74 @@ class HomePage(tk.Frame):
         self.controller = controller
 
         orig_width = 1455
-        orig_height = 1041
+        orig_height = 1040
+        self.orig_width = orig_width
+        self.orig_height = orig_height
         new_width = 800
         new_height = 600
-        scale_x = new_width / orig_width
-        scale_y = new_height / orig_height
+        self.scale_x = new_width / orig_width
+        self.scale_y = new_height / orig_height
 
-        canvas = Canvas(self, bg="#FFFFFF", height=orig_height, width=orig_width,
+        self.canvas = Canvas(self, bg="#FFFFFF", height=orig_height, width=orig_width,
                         bd=0, highlightthickness=0, relief="ridge")
-        canvas.pack(fill="both", expand=True)
-        
-        canvas.create_rectangle(0.0, 0.0, 235.0* scale_x, 1042.0, fill="#79BCF7", outline="")
+        self.canvas.pack(fill="both", expand=True)
 
-# Navigation button: switch page
+        self.canvas.create_rectangle(0.0, 0.0, 235.0, 1042.0, fill="#79BCF7", outline="")
+       # Navigation button: switch page
        # ----------------------------HomePage------------------------------------------
-        btn5_img = scaled_photoimage(str(relative_to_assets("button_5.png")), scale_x, scale_y)
+        btn5_img = scaled_photoimage(str(relative_to_assets("button_5.png")), self.scale_x, self.scale_y)
         btn5 = Button(self, image=btn5_img, borderwidth=0, highlightthickness=0,
-                      command=lambda: controller.show_frame("HomePage"), relief="flat")
+                      command=lambda: self.controller.show_frame("HomePage"), relief="flat")
         btn5.image = btn5_img
-        btn5.place(x=0.0 * scale_x, y=2 * scale_y, width=235.0 * scale_x, height=100.0 * scale_y)  
+        btn5.place(x=0.0 * self.scale_x, y=2 * self.scale_y, width=235.0 * self.scale_x, height=100.0 * self.scale_y)  
        # ----------------------------UploadPage------------------------------------------
-        btn4_img = scaled_photoimage(str(relative_to_assets("button_4.png")), scale_x, scale_y)
+        btn4_img = scaled_photoimage(str(relative_to_assets("button_4.png")), self.scale_x, self.scale_y)
         btn4 = Button(self, image=btn4_img, borderwidth=0, highlightthickness=0,
-                      command=lambda: controller.show_frame("UploadPage"), relief="flat")
+                      command=lambda: self.controller.show_frame("UploadPage"), relief="flat")
         btn4.image = btn4_img
-        btn4.place(x=0.0 * scale_x, y=102.0 * scale_y, width=235.0 * scale_x, height=100.0 * scale_y)
+        btn4.place(x=0.0 * self.scale_x, y=102.0 * self.scale_y, width=235.0 * self.scale_x, height=100.0 * self.scale_y)
         # ----------------------------viewPage------------------------------------------
-        btn4_img = scaled_photoimage(str(relative_to_assets("button_2.png")), scale_x, scale_y)
+        btn4_img = scaled_photoimage(str(relative_to_assets("button_2.png")), self.scale_x, self.scale_y)
         btn4 = Button(self, image=btn4_img, borderwidth=0, highlightthickness=0,
-                      command=lambda: controller.show_frame("ViewPageOverall"), relief="flat")
+                      command=lambda: self.controller.show_frame("ViewPageOverall"), relief="flat")
         btn4.image = btn4_img
-        btn4.place(x=0.0 * scale_x, y=202.0 * scale_y, width=235.0 * scale_x, height=100.0 * scale_y)     
+        btn4.place(x=0.0 * self.scale_x, y=202.0 * self.scale_y, width=235.0 * self.scale_x, height=100.0 * self.scale_y)     
        # ----------------------------StartPage------------------------------------------
-        btn3_img = scaled_photoimage(str(relative_to_assets("button_3.png")), scale_x, scale_y)
+        btn3_img = scaled_photoimage(str(relative_to_assets("button_3.png")), self.scale_x, self.scale_y)
         btn3 = Button(self, image=btn3_img, borderwidth=0, highlightthickness=0,
-                      command=lambda: controller.show_frame("StartPage"), relief="flat")
+                      command=lambda: self.controller.show_frame("StartPage"), relief="flat")
         btn3.image = btn3_img
-        btn3.place(x=0.0 * scale_x, y=302.0 * scale_y, width=235.0 * scale_x, height=100.0 * scale_y)
+        btn3.place(x=0.0 * self.scale_x, y=302.0 * self.scale_y, width=235.0 * self.scale_x, height=100.0 * self.scale_y)
        # ----------------------------ExportPage------------------------------------------
-        btnexport_img = scaled_photoimage(str(relative_to_assets("export_button.png")), scale_x, scale_y)
+        btnexport_img = scaled_photoimage(str(relative_to_assets("export_button.png")), self.scale_x, self.scale_y)
         btnexport = Button(self, image=btnexport_img, borderwidth=0, highlightthickness=0,
-                      command=lambda: controller.show_frame("ExportPage"), relief="flat")
+                      command=lambda: self.controller.show_frame("ExportPage"), relief="flat")
         btnexport.image = btnexport_img
-        btnexport.place(x=0.0 * scale_x, y=402.0 * scale_y, width=235.0 * scale_x, height=100.0 * scale_y)
+        btnexport.place(x=0.0 * self.scale_x, y=402.0 * self.scale_y, width=235.0 * self.scale_x, height=100.0 * self.scale_y)
         # ----------------------------SettingPage------------------------------------------
-        btn1_img = scaled_photoimage(str(relative_to_assets("button_1.png")), scale_x, scale_y)
+        btn1_img = scaled_photoimage(str(relative_to_assets("button_1.png")), self.scale_x, self.scale_y)
         btn1 = Button(self, image=btn1_img, borderwidth=0, highlightthickness=0,
-                      command=lambda: controller.show_frame("SettingPage"), relief="flat")
+                      command=lambda: self.controller.show_frame("SettingPage"), relief="flat")
         btn1.image = btn1_img
-        btn1.place(x=0.0 * scale_x, y=502.0 * scale_y, width=235.0 * scale_x, height=100.0 * scale_y)
+        btn1.place(x=0.0 * self.scale_x, y=502.0 * self.scale_y, width=235.0 * self.scale_x, height=100.0 * self.scale_y)
                 
         #logo iamge
-        img1 = scaled_photoimage(str(relative_to_assets("image_1.png")), scale_x, scale_y)
-        canvas.create_image(215.0 * scale_x, 1700.0 * scale_y, image=img1)
-        canvas.image = img1
+        img1 = scaled_photoimage(str(relative_to_assets("image_1.png")), self.scale_x, self.scale_y)
+        self.canvas.create_image(215.0 * self.scale_x, 1700.0 * self.scale_y, image=img1)
+        self.canvas.image = img1
 
-        canvas.create_rectangle(255.0* scale_x, 20.0 * scale_y, 1435.0* scale_x, 1020.0* scale_y, fill="#DAEBF9", outline="")
-        #canvas.scale("all", 0, 0, scale_x, scale_y)
+        self.bind("<Configure>", self.on_resize)
+        self.prev_width = self.orig_width
+        self.prev_height = self.orig_height
+        self.make_GUI()
+    
+    def make_GUI(self):
+        self.canvas.create_rectangle(255.0* self.scale_x, 20.0 * self.scale_y, 1435.0* self.scale_x, 1020.0* self.scale_y, fill="#DAEBF9", outline="")
+        #self.canvas.scale("all", 0, 0, self.scale_x, self.scale_y)
 
-        canvas.create_text(486.2366 * scale_x, 95.0247 * scale_y,
+        self.canvas.create_text(486.2366 * self.scale_x, 95.0247 * self.scale_y,
                            anchor="nw", text="Welcome to Course Scheduling System",
-                           fill="#094478", font=("Roboto Black", int(30 * scale_y)))
+                           fill="#094478", font=("Roboto Black", int(30 * self.scale_y)))
         desc_text = (
             "                                             System Description:\n\n"
             "Make a compatible easy-to-use user interface within an application that allows\n"
@@ -113,8 +120,27 @@ class HomePage(tk.Frame):
             "8. Click 'Export' to export schedule as a .csv and .pdf\n"
             "    For additional help, hover over buttons and fields for tooltips!"
         )
-        canvas.create_text(366.4585 * scale_x, 234.97 * scale_y,
+        self.canvas.create_text(366.4585 * self.scale_x, 234.97 * self.scale_y,
                            anchor="nw", text=desc_text, fill="#094478",
-                           font=("Roboto Regular", int(20 * scale_y)))
-        
+                           font=("Roboto Regular", int(20 * self.scale_y)))
+
+
+    def clear_gui(self):
+        self.canvas.delete("all")
+
+    def on_resize(self, event):
+        if event.width < 300 or event.height < 300:
+            return  # prevent errors on small resize
+
+        scale_x = event.width / self.orig_width
+        scale_y = event.height / self.orig_height
+
+        if (event.width, event.height) != (self.prev_width, self.prev_height):
+            self.scale_y = scale_y
+            self.scale_x = scale_x
+            self.prev_width = event.width
+            self.prev_height = event.height
+            self.clear_gui()
+            self.make_GUI()
+
 
